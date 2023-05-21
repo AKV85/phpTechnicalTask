74e5af2c8e6256cf05f6cd2059ea893b03de7842
AKV85 <a.kotov.laknojus@gmail.com>
2023-05-21 16:23:40 +0300

    Create ContactRepository

---
+++ app/Http/Repositories/ContactRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Repositories;
+
+use App\Models\Contact;
+use Illuminate\Support\Facades\DB;
+
+class ContactRepository
+{
+    public function getAllContacts()
+    {
+        return Contact::latest()->paginate(10);
+    }
+
+    public function createContact($data)
+    {
+        return Contact::create($data);
+    }
+
+    public function getContactById($id)
+    {
+        return Contact::find($id);
+    }
+
+    public function updateContact($contact, $data)
+    {
+        return $contact->update($data);
+    }
+
+    public function deleteContact($contact)
+    {
+        return $contact->delete();
+    }
+
+    public function getContactsWhereMessageLengthGreaterThan($length)
+    {
+        return DB::table('contacts')
+            ->whereRaw('CHAR_LENGTH(message) > ?', [$length])
+            ->get();
+    }
+}
